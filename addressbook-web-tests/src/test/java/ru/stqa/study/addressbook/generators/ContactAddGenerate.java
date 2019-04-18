b630309351014dbd30ba3464d9e2d4b246b3dfda
AnnaPyatnitskaya <antares1990@mail.ru>
2019-04-18 19:48:38 +0300

    Генератор тестовых данных для групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/study/addressbook/generators/ContactAddGenerate.java
+++ addressbook-web-tests/src/test/java/ru/stqa/study/addressbook/generators/ContactAddGenerate.java
@@ -1,6 +1,10 @@
 package ru.stqa.study.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.study.addressbook.model.ContactAdd;
+import ru.stqa.study.addressbook.model.GroupDate;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,13 +15,28 @@ import java.util.List;
 
 public class ContactAddGenerate {
 
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
     public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
+        ContactAddGenerate generator = new ContactAddGenerate();
+        JCommander jCommander= new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
 
+    }
+
+    private void run() throws IOException {
         List<ContactAdd> contacts = generateContacts(count);
-        save(contacts, file);
-
+        save(contacts, new File(file));
     }
 
     private static void save(List<ContactAdd> contacts, File file) throws IOException {
