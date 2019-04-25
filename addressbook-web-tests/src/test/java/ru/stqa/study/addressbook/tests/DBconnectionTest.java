915983338b54262c8cc58e5026f1d102629dc9f1
AnnaPyatnitskaya <antares1990@mail.ru>
2019-04-25 11:36:42 +0300

    Добавление в конфигурацию адреса БД

--- addressbook-web-tests/src/test/java/ru/stqa/study/addressbook/tests/DBconnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/study/addressbook/tests/DBconnectionTest.java
@@ -1,40 +1,48 @@
 package ru.stqa.study.addressbook.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 import ru.stqa.study.addressbook.model.GroupDate;
 import ru.stqa.study.addressbook.model.Groups;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Connection;
 
 import java.sql.*;
 
+
 public class DBconnectionTest {
 
-    @Test
-    public void testDbConnection() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("select group_id, group_name, group_footer, group_header from group_list");
-            Groups groups = new Groups();
-            while (rs.next()) {
-                new GroupDate().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
-                        .withFooter(rs.getString("group_footer")).withHeader(rs.getString("group_header"));
+
+    public class LoadDriver {
+
+        @Test
+        public void testDbConnection() {
+            Connection conn = null;
+            try {
+                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&serverTimezone=UTC");
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery("select group_id, group_name, group_footer, group_header from group_list");
+                Groups groups = new Groups();
+                while (rs.next()) {
+                    new GroupDate().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                            .withFooter(rs.getString("group_footer")).withHeader(rs.getString("group_header"));
+                }
+                rs.close();
+                st.close();
+                conn.close();
+
+                System.out.println(groups);
+
+                // Do something with the Connection
+
+            } catch (SQLException ex) {
+                // handle any errors
+                System.out.println("SQLException: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("VendorError: " + ex.getErrorCode());
             }
-            rs.close();
-            st.close();
-            conn.close();
-
-            System.out.println(groups);
-
-            // Do something with the Connection
-
-        } catch (SQLException ex) {
-            // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
         }
     }
 }
